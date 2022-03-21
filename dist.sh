b020c9456e5c9556579f7901b54af7ad57c536b7
cyberstudio10 <13622461+cyberstudio10@users.noreply.github.com>
2022-03-21 05:47:57 -0700

    To the user, a timestamp may be more helpful than a git commit id to identify the version of the cIOS.

--- dist.sh
+++ dist.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Copyright (C) 2020 Leseratte
+# Copyright (C) 2022 cyberstudio
+
 # This bash script is running inside the Docker container
 # to actually compile the cIOS. It first creates the
 # stripios binary, then runs the maked2x script, while
-# including the git commit ID in the version string.
+# including the git commit timestamp in the version string.
 
 # See wiki, this is needed to be able to compile outside of Github environment.
 cd /docker-mountpoint || /bin/true
@@ -15,6 +18,6 @@ cd ..
 timestamp=$(git log -1 --pretty=%ct)
 echo Timestamp is $timestamp
 export SOURCE_DATE_EPOCH=$timestamp 
-export D2XL_VER_COMPILE="v9-git+$(git rev-parse --short HEAD)"
-./maked2x.sh 9 git+$(git rev-parse --short HEAD)
+export D2XL_VER_COMPILE="v9-$(git log -1 --format=%cd --date=format:%Y%m%d%H%M)"
+./maked2x.sh 9 $(git log -1 --format=%cd --date=format:%Y%m%d%H%M)
 
