4bec184c412e8ba7196880ff3eeca8da946065a2
cyberstudio10 <13622461+cyberstudio10@users.noreply.github.com>
2022-03-23 00:19:43 -0700

    To achieve binary reproducibility the build shall only depend on git HEAD. There are only 2 ways to achieve this. The first is to use a decimal timestamp with 12 digits (6 date + 6 time to the seconds). The second is to use the 7 hex digit commit hash. I think the former is more informative, but too long when displayed on the screen. I will go back to the latter.

--- dist.sh
+++ dist.sh
@@ -6,7 +6,7 @@
 # This bash script is running inside the Docker container
 # to actually compile the cIOS. It first creates the
 # stripios binary, then runs the maked2x script, while
-# including the git commit timestamp in the version string.
+# including the git commit ID in the version string.
 
 # See wiki, this is needed to be able to compile outside of Github environment.
 cd /docker-mountpoint || /bin/true
@@ -18,7 +18,11 @@ cd ..
 timestamp=$(git log -1 --pretty=%ct)
 echo Timestamp is $timestamp
 export SOURCE_DATE_EPOCH=$timestamp 
-MINOR_VERSION=.0.${GITHUB_RUN_NUMBER:-unknown}
+# The build shall reproduce the exact same binaries no matter who runs it. This
+# requires a versioning mechanism that only depends on git HEAD. We use the
+# 7-hex-digit commit hash because it is the shortest thing that can be displayed
+# on screen that uniquely identifies the build.
+MINOR_VERSION=+$(git rev-parse --short HEAD)
 export D2XL_VER_COMPILE="v9${MINOR_VERSION}"
 ./maked2x.sh 9 ${MINOR_VERSION}
 
